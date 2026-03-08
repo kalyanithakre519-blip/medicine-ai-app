a1ac0a6b402bee7eae649a4764d59c232d393ef7
Kalyani Thakre <kalyanithakre519@gmail.com>
2026-03-08 20:00:01 +0530

    Fix API URL configuration for Vercel deployment

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fsupd00hsh8"
+    "revision": "0.a0aa3t7g9o8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
